49854f32abb486d0c27af447086c1b8b21d915b5
Atsushi-Kakuta <invest9me@yahoo.co.jp>
2022-10-11 15:53:03 +0900

    first django projectg

---
+++ polls/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+                      {
+                          'question': question,
+                          'error_message': 'You`re didn`t select a choice.'
+                      })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    return HttpResponse(reverse('polls:result', args=(question_id,)))
